803e0944f30dd6d49e1bd0a8919af30acb74b145
Valentina Burbano <valentina@portfolio.dev>
2026-04-22 07:15:53 -0500

    Update src/components/index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,12 @@
 export { Navbar, Footer } from './layout';
 export { Button, AnimatedBorderButton } from './common';
 export {
-	Hero,
-	About,
-	Experience,
-	Projects,
-	Testimonials,
-	ServicesAndFAQ,
-	ProcessAndGrowth,
+  Hero,
+  About,
+  Experience,
+  Projects,
+  Testimonials,
+  ServicesAndFAQ,
+  ProcessAndGrowth,
+  Contact,
 } from './sections';
